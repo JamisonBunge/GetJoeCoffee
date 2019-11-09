780c3d706e1f2313416e45af16e5112a7b75290a
Jamison Bunge <JamisonBunge@outlook.com>
2019-11-09 18:54:32 -0500

    Correctly using Places API, sorting places into three categories

--- backend/DataSources/Places.js
+++ backend/DataSources/Places.js
@@ -7,15 +7,60 @@ class Places extends RESTDataSource {
         //this.baseURL = "";
     }
     willSendRequest(request) {
-        request.headers.set('Client-ID', this.ClientID);
+        //request.headers.set('key', this.apiKey);
     }
-    async testAPI() {
-        var result = await this.get(" https://maps.googleapis.com/maps/api/place/findplacefromtext/output?parameters")
-        console.log(result)
-        return result.data
+    async usePlacesAPI() {
+        var result = await this.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=40.807537,-73.962570&radius=1500&type=cafe&&key=" + this.ClientID)
+        //console.log("https://maps.googleapis.com/maps/api/place/textsearch/json?query=123+main+street.i&location=42.3675294,-71.186966&radius=1000&key" + this.ClientID)
+        //console.log("THIS:");
+        //  console.log(result)
+        let nearbyPlaces = result.results;
+
+        // Using results of the Places API nearby results, create our own objects to use
+        // containing only the relevant data provided that we need, such as place name,
+        // latitude, longitude, type(s).
+        // All of this will go into the array called places, representing the simpler
+        // objects representing nearby places.
+        let places = [];
+        for (let entry of nearbyPlaces) {
+            let place = new Object();
+            place.name = entry.name;
+            place.lat = entry.geometry.location.lat;
+            place.lng = entry.geometry.location.lng;
+            place.types = entry.types;
+            places.push(place);
+            // console.log(place + "\n")
+        }
+
+        // Now we need to categorize the nearby places into one of the three categories (quick, sitdown, or chain).
+        // A chain is either Starbucks, McDonalds, or Dunkins, regardless of the types. We make this judgement based on name.
+        // A sitdown place is a place that is both a cafe and a restaurant type.
+        // A quick place is a place that is only type cafe, not type restaraunt.
+        let bigChains = ["McDonald's", "Starbucks", "Dunkin'"];
+        let chains = [];
+        let sitdowns = [];
+        let quicks = [];
+
+        // Loop through places and categorize as we go!
+        // If its a big chain, immediately add it to chains.
+        // Next, check if it includes restaurant in its type list. If so, add it to sitdowns.
+        // Else, (if it is just a cafe type), add it to quicks.
+        for (let place of places) {
+            if (bigChains.includes(place.name)) {
+                chains.push(place);
+            } else if (place.types.includes('restaurant')) {
+                sitdowns.push(place);
+            } else {
+                quicks.push(place);
+            }
+        }
+        // for (let x of chains) { process.stdout.write(x.name + ", ") }
+        return { "chainList": chains, "sitdownList": sitdowns, "quickList": quicks }
     }
-    getPlaces() {
-        this.testAPI();
+    async getPlaces() {
+        let collections = await this.usePlacesAPI();
+        console.log(collections);
+
         return { "quick": "starbucks", "hipster": "joes", "sitdown": "new york basics" }
     }
 
