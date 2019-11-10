326dfbf139b53b06a78e1da7eff886ef0adef1f8
Jamison Bunge <JamisonBunge@outlook.com>
2019-11-09 22:42:12 -0500

    setting up node for hosting

--- backend/App.js
+++ backend/App.js
@@ -39,6 +39,10 @@ const server = new ApolloServer({
 	})
 });
 
-server.listen().then(({ url }) => {
-	console.log(`Server ready at ${url}`);
+// server.listen().then(({ url }) => {
+// 	console.log(`Server ready at ${url}`);
+// });
+
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+	console.log(`🚀 Server ready at ${url}`);
 });
