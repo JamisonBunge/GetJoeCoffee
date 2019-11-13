942773fd93337f4f1a95e87cc739c2c66571b3fc
Jamison Bunge <JamisonBunge@outlook.com>
2019-11-12 21:50:48 -0500

    set up for hosting

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
-const { ApolloServer, gql } = require('apollo-server');
+const { ApolloServer, gql } = require('apollo-server-express');
+const express = require('express');
 const Places = require('./DataSources/Places')
+const path = require('path')
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
+
 
 const schema = gql`
 type Query {
@@ -31,6 +38,8 @@ const resolvers = {
 
 
 
+
+
 const server = new ApolloServer({
 	typeDefs: schema,
 	resolvers,
@@ -43,6 +52,21 @@ const server = new ApolloServer({
 // 	console.log(`Server ready at ${url}`);
 // });
 
-server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
-	console.log(`🚀 Server ready at ${url}`);
-});
+// server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+// 	console.log(`🚀 Server ready at ${url}`);
+// });
+
+
+//
+app.use(express.static('public'))
+
+//redirect everything that isnt to /graphql to the index.thml of the react app build
+
+
+// Add the Apollo Server’s middleware
+server.applyMiddleware({ app })
+
+app.get('*', (req, res) => {
+	res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+})
+app.listen({ port: process.env.PORT || 5000 }, () => console.log(`Apollo Server is listening on ${server.graphqlPath}`))
